d840ceb276840ab1fd93273e972c9b1c28b5cc4d
Jenaide <jenaidesibolie@gmail.com>
2023-01-04 19:22:06 +0000

    initial commit to print alphabt

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Write a program that prints the ASCII alphabet, in lowercase, not followed by a new line."""
+"""Write a program that prints the ASCII alphabet, in lowercase."""
 
 for i in range(97, 123):
 	print("{}".format(chr(i)), end="")
