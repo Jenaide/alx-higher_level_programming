865df869e46f234b223106b59c7960297d0aebab
Jenaide <jenaidesibolie@gmail.com>
2023-01-04 20:13:07 +0000

    bug fix in upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 
 def uppercase(str):
     for c in str:
-        char = ird(c)
+        char = ord(c)
         if char >= 97 and char <= 122:
             char -= 32
         print("{:c}".format(char), end="")
