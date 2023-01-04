ad57ecad1d1d2e1d695bf10735dbb47d65cc4ef1
Jenaide <jenaidesibolie@gmail.com>
2023-01-04 20:44:47 +0000

    initial commit to insert

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts a node sorted in a linked list
+ * @head: double pointer to head of link list
+ * number: data for new node
+ * Return: pointer to new node ,NULL on failure
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head, *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
+}
