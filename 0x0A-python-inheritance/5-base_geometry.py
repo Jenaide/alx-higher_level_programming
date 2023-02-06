8a246550bce9e75c0d572d7ed517e69d9532d6a2
Jenaide <jenaidesibolie@gmail.com>
2023-02-06 19:47:14 +0000

    task 5 and 6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author: Jenaide Sibolie
+"""
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author: Jenaide Sibolie
+"""
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    pass
+
+    def area(self):
+        """
+        Public instance method that calculates the area
+        Raises:
+            Exception if area is not imlemented
+        """
+        raise Exception("area() is not implemented")
