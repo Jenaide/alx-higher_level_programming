e0959090550c34dc2f6e60991562f5f36f7d3d53
Jenaide <jenaidesibolie@gmail.com>
2023-02-08 19:08:07 +0000

    bug fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,13 +6,12 @@ Created on Wed Feb 8, 2023
 """
 
 
-Basegeometry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """
-    A Rectangle class shape, inherits from 
-    BaseGeometry.
+    A Rectangle class shape, inheirts from BaseGeometry
     """
     def __init__(self, width, height):
         self.integer_validator("width", width)
