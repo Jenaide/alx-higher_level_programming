df36b684c0b4ec6337987daf71362df3b3d1b3c8
Jenaide <jenaidesibolie@gmail.com>
2023-04-11 19:34:51 +0000

    added files 2 to 13

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const arg = process.argv.length;
+if (arg > 3) {
+  console.log('Arguments found');
+} else if (arg === 3) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}
