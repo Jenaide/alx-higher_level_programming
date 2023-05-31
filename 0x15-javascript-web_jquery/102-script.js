d0ff0e3d8913a61e5af4e0a8534199dfc8482b22
Jenaide <jenaidesibolie@gmail.com>
2023-05-31 13:02:46 -0700

    added additional tasks and mains

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,11 @@
+const $ = window.$;
+
+$(function() {
+  $('INPUT#btn_translate').click(function() {
+    const languageCode = $('INPUT#language_code').val();
+
+    $.get('https://www.fourtonfish.com/hellosalut/hello/', { lang: languageCode }, function(data) {
+      $('DIV#hello').text(data.hello);
+    });
+  });
+});
---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,14 @@
+const $ = window.$;
+
+$(function() {
+  $('INPUT#btn_translate, INPUT#language_code').on('click keypress', function(data) {
+    if (data.type === 'click' || (data.type === 'keypress' && data.which === 13)) {
+      const languageCode = $('INPUT#language_code').val();
+      const url = 'https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}';
+      
+      $.get(url, function(greet) {
+        $('DIV#hello').text(greet.hello);
+      });
+    }
+  });
+});
