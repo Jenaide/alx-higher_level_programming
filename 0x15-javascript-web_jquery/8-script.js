30c19479d28605e231c98ce9c9b983e1effd299c
Jenaide <jenaidesibolie@gmail.com>
2023-05-31 12:17:11 -0700

    added all files including mains

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+const $ = window.$;
+const url = 'https://swapi-api.alx-tools.com/api/films/?format=json'
+
+$.getJSON(url, function (data) {
+  let titles = data.titles;
+  $('UL#list_movies').text(data);
+});
